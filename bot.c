bfe1cb86e69cce698ff57d9b210d7dc1be60e45c
Yasmin-Barros <yasmin.barros@uol.com.br>
2024-10-18 14:51:31 -0300

    Update bot.c

--- bot.c
+++ bot.c
@@ -1,11 +1,9 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-//tamanho do tabuleiro (10x10)
-#define SIZE 10  
-// qtd de navios dele
-#define NUM_NAVIOS 7
+#define SIZE 10
+#define NUM_NAVIOS_BOT_BOT 10 // AUMENTEI PRA 10
 
 // Estrutura para armazenar a posição do navio
 typedef struct {
@@ -13,40 +11,41 @@ typedef struct {
     int y;
 } Coordenada;
 
-void posicaoBot() {
+
+
+// Função para gerar a posição aleatória dos navios do bot
+void posicaoBot(Coordenada navios[], char tabuleiroBotInvisivel[SIZE][SIZE]) {
     int x, y;
     int i = 0;
-    srand(time(NULL)); // Números aeatorios
+    srand(time(NULL)); 
 
-    while (i < NUM_NAVIOS) {
+    while (i < NUM_NAVIOS_BOT) {
         x = rand() % SIZE; // Gera uma coordenada x aleatória
         y = rand() % SIZE; // Gera uma coordenada y aleatória
         int posLivre = 1;
 
         // Verifica se a posição já está ocupada
-        for (int j = 0; j < i; j++) {
-            if (navios[j].x == x && navios[j].y == y) {
-                posLivre = 0;
-                break;
-            }
+        if (tabuleiroBotInvisivel[x][y] == 'X') {
+            posLivre = 0;
         }
 
         if (posLivre) {
-            navios[i].x = x - 1;
-            navios[i].y = y - 1;
+            navios[i].x = x;
+            navios[i].y = y;
+                tabuleiroBotInvisivel[x][y] = 'X'; // Marca a posição do navio no tabuleiro vulgo na matriz
             i++;
         }
     }
 }
 
-// ataque aleatório
+// Função para o bot atacar (jogar aleatoriamente)
 void ataqueBot(Coordenada ataques[], int *numAtaques) {
     int x, y;
     int posLivre = 1;
     srand(time(NULL));
 
     do {
-        posLivre = 1; //posição livre
+        posLivre = 1;
         x = rand() % SIZE; // Gera uma coordenada x aleatória
         y = rand() % SIZE; // Gera uma coordenada y aleatória
 
@@ -59,8 +58,8 @@ void ataqueBot(Coordenada ataques[], int *numAtaques) {
         }
     } while (!posLivre);
 
-    ataques[*numAtaques].x = x -1;
-    ataques[*numAtaques].y = y-1;
+    ataques[*numAtaques].x = x;
+    ataques[*numAtaques].y = y;
     (*numAtaques)++;
 }
 
