a1c8318641d1cd31d019e5652ec03e06497432ff
Yasmin-Barros <yasmin.barros@uol.com.br>
2024-10-18 14:52:10 -0300

    Update bot.h

--- bot.h
+++ bot.h
@@ -2,14 +2,14 @@
 #define bot_h
 
 #define SIZE 10
-#define NUM_NAVIOS 7
+#define NUM_NAVIOS_BOT 10
 
 typedef struct {
     int x;
     int y;
 } Coordenada;
 
-void botPosicao(Coordenada navios[]);  // Declaração da função
+void botPosicao(Coordenada navios[], char tabuleiroBotInvisivel[SIZE][SIZE]);  // Declaração da função
 
 void botAtaque(Coordenada ataques[], int *numAtaques);   // Declaração 
 
